632a74b7c4c98c77e00aa212405a8ab2780f2b39
leoh86 <162754877+leoh86@users.noreply.github.com>
2024-03-08 16:27:10 -0600

    Añadí otra forma de este conversor

--- 3_Libras_a_euros.py
+++ 3_Libras_a_euros.py
@@ -11,3 +11,21 @@ conversion = libra * euro
 print("{} libra(s) son {} euro(s)".format(libra,conversion))
 
 print("Gracias por utilizar mi programa :)")
+
+"""
+###
+    Agrego aquí otra forma de hacerlo, en este el usuario introduce el valor de la moneda en ese día, lo ideal sería juntarlos por que,
+    el otro tiene mejor interfaz con el usuario.
+
+print("Hola!, este es un conversor de Libras esterlinas a euros")
+l = float(input("¿Cuántas libras esterlinas quieres convertir a euros? "))
+c = float(input("¿Cuál es el valor del euro respecto a la libra esterlina? "))
+
+print("1 libra = {} euros".format(c))
+conversión = l * c
+
+print("{} libras son {} euros".format(l, conversión))
+
+print("Gracias por usar este conversor :)")
+
+"""
