22de07c16a914015cae89a35b5e1772cd408eafd
leoh86 <162754877+leoh86@users.noreply.github.com>
2024-03-08 16:11:46 -0600

    Creé mi primer programa

---
+++ Primer código.py
@@ -0,0 +1,21 @@
+numero_1 = int(input("Hola, este es mi primer programa de python, ¿Me harías el favor de introducir 3 números? Va, elige un número entero, no importa que tan grande o pequeño sea: "))
+print("Tu primer número es: ")
+print(numero_1)
+
+numero_2 = int(input("Gracias, introduce un segundo número: "))
+print("Tu segundo número es:")
+print(numero_2)
+
+numero_3 = int(input("Perfecto, introduce un tercer número: "))
+print("Tu tercer número es: ")
+print(numero_3)
+
+print("Ahora, voy a hacer un poquito de magia de segundo de primaria y decirte el número mayor y menor, ¿Listo?")
+
+print("El número máyor es:")
+print(max(numero_1,numero_2, numero_3))
+
+print("El número menor es:")
+print(min(numero_1, numero_2, numero_3))
+
+print("Gracias por usar mi primer programa <3")
