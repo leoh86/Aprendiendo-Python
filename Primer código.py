7a5920987298e173392261533d227a2721abc640
leoh86 <162754877+leoh86@users.noreply.github.com>
2024-03-08 16:12:44 -0600

    Mejoré mi primer programa usando .format

--- Primer código.py
+++ Primer código.py
@@ -1,21 +1,20 @@
-numero_1 = int(input("Hola, este es mi primer programa de python, ¿Me harías el favor de introducir 3 números? Va, elige un número entero, no importa que tan grande o pequeño sea: "))
-print("Tu primer número es: ")
-print(numero_1)
 
-numero_2 = int(input("Gracias, introduce un segundo número: "))
-print("Tu segundo número es:")
-print(numero_2)
 
-numero_3 = int(input("Perfecto, introduce un tercer número: "))
-print("Tu tercer número es: ")
-print(numero_3)
+numero1 = int(input("Hola, este es mi primer programa de python, ¿Me harías el favor de introducir 3 números? Va,"
+                    " elige un número entero no importa que tan grande o pequeño sea: "))
+print("Tu primer número es: {}".format(numero1))
+
+numero2 = int(input("Gracias, introduce un segundo número: "))
+print("Tu segundo número es: {}".format(numero2))
+
+numero3 = int(input("Perfecto, introduce un tercer número: "))
+print("Tu tercer número es: {}".format(numero3))
 
 print("Ahora, voy a hacer un poquito de magia de segundo de primaria y decirte el número mayor y menor, ¿Listo?")
 
-print("El número máyor es:")
-print(max(numero_1,numero_2, numero_3))
+print("El número máyor entre {}, {} y {} es: {}".format(numero1, numero2, numero3, max(numero1,numero2, numero3)))
 
-print("El número menor es:")
-print(min(numero_1, numero_2, numero_3))
+print("El número menor entre {}, {} y {} es: {}".format(numero1, numero2, numero3, min(numero1,
+                                                        numero2, numero3)))
 
 print("Gracias por usar mi primer programa <3")
